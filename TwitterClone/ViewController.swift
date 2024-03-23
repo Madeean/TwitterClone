21df40ec4ff364578f5a775802eb06813ee71041
MadeReihan <reihanmade@gmail.com>
2024-03-23 20:33:46 +0700

    Initial Commit

---
+++ TwitterClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by made reihan on 23/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
